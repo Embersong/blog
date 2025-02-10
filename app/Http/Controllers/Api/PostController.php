2319e3c986c2535a659a1589b4de0c8ab89258b8
ember <evebay@mail.ru>
2025-02-11 02:05:03 +0700

    api sanctum

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+
+        return PostResource::collection(Post::with('category')->get());
+
+ /*       $response = [
+            'success' => true,
+            'message' => 'List all posts',
+            'data' => $posts,
+        ];
+
+        return response()->json($response, 200);*/
+
+
+    }
+
+    public function show($id) {
+        $post = Post::findOrFail($id);
+
+        return (new PostResource($post))->additional([
+            'success' => true,
+            'message' => 'Posts retrieved successfully'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//REST HTTP
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show'])->middleware('auth:sanctum');
